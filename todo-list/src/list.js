ecf969bd59e88412406abcdb2ab1d94f89ef45d2
kingduidui <m18508218948@163.com>
2018-06-07 16:36:53 +0800

    todolist

---
+++ todo-list/src/list.js
@@ -0,0 +1,121 @@
+export default  [
+    {
+      task: "做一个todoList",
+      complete: true
+    },        
+    {
+      task: "ES6/ES7 语法和原理实现",
+      complete: false
+    }, {
+      task: "异步发展过程：callback/promise/generator/co/async/await等异步流程控制",
+      complete: false
+    }, {
+      task: "JavaScript模块化发展史: CommonJS/AMD/CMD/ES6模块",
+      complete: false
+    }, {
+      task: "promise设计模式原理及在es6中的应用,手写一个符合promise A+ 规范的promise实现",
+      complete: false
+    }, {
+      task: "Nodes 核心原理， 手写Stream的源码",
+      complete: false
+    }, {
+      task: "Node事件原理和发布订阅设计模式",
+      complete: false
+    }, {
+      task: "文件系统遍历算法（同步、异步、深度、广度、先序、中序、后续）",
+      complete: false
+    }, {
+      task: "用TCP实现HTTP和Websocket协议",
+      complete: false
+    }, {
+      task: "手写一款HTTP服务命令行工具，包括压缩、缓存、断点续传、文件上传等功能",
+      complete: false
+    }, {
+        task: "Cookie和Session原理",
+        complete: false
+    }, {
+        task: "webpack4实战",
+        complete: false
+    }, {
+        task: "webpack优化(resolve、模块热替换、压缩、代码分隔、可视化工具)",
+        complete: false
+    }, {
+        task: "webpack4源码分析",
+        complete: false
+    }, {
+        task: "编写自定义loader、编写自定义Plugin",
+        complete: false
+    }, {
+        task: "webpack的事件机制tapable和AST抽象语法树",
+        complete: false
+    }, {
+        task: "如何编写Babel插件",
+        complete: false
+    }, {
+        task: "手写一款简易webpack",
+        complete: false
+    }, {
+        task: "react 全家桶",
+        complete: false
+    }, {
+        task: "vue 全家桶",
+        complete: false
+    }, {
+        task: "react 和 vue 源码解析并手写一款自己的mvvm框架",
+        complete: false
+    }, {
+        task: "http 和 tcp 的深入",
+        complete: false
+    }, {
+        task: "express，koa的手写，中间件的原理实现",
+        complete: false
+    }, {
+        task: "egg.js + antdesign开发前后端分离应用",
+        complete: false
+    }, {
+        task: "linux",
+        complete: false
+    }, {
+        task: "Mongodb",
+        complete: false
+    }, {
+        task: "个人博客搭建( express + mongodb )",
+        complete: false
+    }, {
+        task: "聊天室项目( react + socket.io + mongodb )",
+        complete: false
+    }, {
+        task: "MySQL",
+        complete: false
+    }, {
+        task: "爬虫",
+        complete: false
+    }, {
+        task: "Redis,Nginx,Docker",
+        complete: false
+    }, {
+        task: "单元测试",
+        complete: false
+    }, {
+        task: "集群和负载均衡",
+        complete: false
+    }, {
+        task: "前端性能监控与性能优化、行为监控与安全防范",
+        complete: false
+    }, {
+        task: "项目部署",
+        complete: false
+    }, {
+        task: "全链路优化",
+        complete: false
+    }, {
+        task: "搞几个实战项目：企业ERP系统，电商APP，Canvas游戏，three.js之微信小游戏，RN项目",
+        complete: false
+    },{
+        task: "看书：JavaScript 设计模式",
+        complete: false
+    },{
+        task: "算法和数据结构",
+        complete: false
+    }
+  ]
