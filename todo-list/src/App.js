dcedbfde0053168faec19ed3ff997ddf03e01dce
kingduidui <m18508218948@163.com>
2018-06-07 18:26:40 +0800

    todolist build

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -25,9 +25,13 @@ class App extends Component {
   item( item, idx ) {
     return (
       <li key = { 'li_' + idx } >
-        <input type = "checkBox" defaultChecked = { item.complete } onClick = { ( e )=>{ this.handleClick( e, idx ) } } id = { 'checkBox_' + idx }/>
-        <label htmlFor = { 'checkBox_' + idx } className = { item.complete ? 'complete' : undefined } >{ item.task }</label>
-        <input type = "button" value = "删除" onClick = { (  )=>{ this.handleDel( idx ) } }/>
+        <div className = "left">
+          <input type = "checkBox" defaultChecked = { item.complete } onClick = { ( e )=>{ this.handleClick( e, idx ) } } id = { 'checkBox_' + idx }/>
+          <label htmlFor = { 'checkBox_' + idx } className = { item.complete ? 'complete' : undefined } >{ item.task }</label>
+        </div>
+        <div className = "right">
+          <input type = "button" value = "删除" onClick = { (  )=>{ this.handleDel( idx ) } }/>
+        </div>
       </li>
     )   
   }
