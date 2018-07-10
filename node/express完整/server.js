e3f5f220f6b9e40d8cce56d39951113516271697
kingduidui <m18508218948@163.com>
2018-07-10 16:45:02 +0800

    consolidate 模板引擎适配

--- node/express完整/server.js
+++ node/express完整/server.js
@@ -4,8 +4,10 @@ const bodyParser = require('body-parser');
 const multer = require('multer');
 const cookieParser = require('cookie-parser');
 const cookieSession = require('cookie-session');
-const jade = require('jade');
-const ejs = require('ejs');
+//const jade = require('jade');
+//const ejs = require('ejs');
+const consolidate = require('consolidate');
+
 
 let server = express();
 let keys = [];
@@ -25,14 +27,26 @@ server.use(cookieSession({
 
 // post数据
 server.use(bodyParser.urlencoded({ extended: false }));
-server.use(multer(__dirname + '/www/upload/').any());
+server.use(multer({ dest: __dirname + '/www/upload/' }).any());
+
+//配置模板引擎
+//1.输出什么东西
+server.set('view engine', 'html');
+//2.模板文件在哪
+server.set('views', __dirname + '/view/');
+//3.哪种模板引擎
+server.engine('html', consolidate.ejs);
 
 // 用户请求
+server.get('/index', (req, res) => {
+    res.render('1.ejs', { name: 'king' }); //注意与send 的区别
+});
+/*
 server.use('/', (req, res, next) => {
     console.log(req.query, req.body, req.files, req.cookies, req.session);
     res.send('ok')
 });
-
+*/
 // static数据
 server.use(expressStatic(__dirname + './www'));
 
